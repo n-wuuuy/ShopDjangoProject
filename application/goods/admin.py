14b3857c8b485a70fddb27b8a162fb3aa315e6c9
n.wuuuy <n.duboisky@gmail.com>
2023-08-11 16:56:53 +0300

    Configured django admin

--- application/goods/admin.py
+++ application/goods/admin.py
@@ -1,16 +1,69 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from goods.models import Goods, GoodsSize, GoodsCategory
+from goods.models import Goods, GoodsSize, GoodsCategory, GoodsImages, Comment
 
 
+class GoodsImagesInline(admin.TabularInline):
+    model = GoodsImages
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Goods)
 class GoodsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'category', 'owner', 'company_name', 'id', 'is_published', 'get_image')
+    list_filter = ('time_create', 'category', 'is_published')
+    search_fields = ('name', 'category__name', 'company_name', 'owner__username')
+    save_on_top = True
+    list_editable = ('is_published',)
+    inlines = [GoodsImagesInline, CommentInline]
+    readonly_fields = ("get_image",)
+    fieldsets = (
+        (None, {
+            "fields": (("name", "slug"),)
+        }),
+        (None, {
+            "fields": (("price", "discount"),)
+        }),
+        (None, {
+            "fields": (("owner", "company_name"),)
+        }),
+        (None, {
+            "fields": (("images", "get_image"),)
+        })
+    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.images.url} width="100" height="110"')
+
+    get_image.short_description = 'Photo'
 
 
+@admin.register(GoodsCategory)
 class GoodsCategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(Goods, GoodsAdmin)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('email', 'goods')
+
+
+@admin.register(GoodsImages)
+class GoodsImagesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'goods', 'get_image')
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
+
+
 admin.site.register(GoodsSize)
-admin.site.register(GoodsCategory, GoodsCategoryAdmin)
